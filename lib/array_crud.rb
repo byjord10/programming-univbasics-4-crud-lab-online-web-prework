1e28fc09baae7ddee704d71738c217c009e63d76
Brianna Jordan <bryvjordan9@gmail.com>
2019-08-05 18:16:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-["Baltimore","DC","Virginia","New Jersery"]
+["Baltimore","DC","Virginia","New Jersey"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+["Baltimore", "DC", "Virginia", "New Jersey"] << "New York"
 end
 
 def add_element_to_start_of_array(array, element)
